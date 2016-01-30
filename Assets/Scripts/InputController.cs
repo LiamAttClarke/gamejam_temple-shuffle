09051a85b4ae82ef77eac73c6ac16acdc49ca756
Ian Nastajus <ian.nastajus@gmail.com>
2016-01-29 23:46:02 -0500

    Playa start

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -3,13 +3,56 @@ using System.Collections;
 
 public class InputController : MonoBehaviour {
 
+    enum inputMode { PLAYER, TILE }
+    inputMode mode = inputMode.PLAYER;
+
+    Player player;
+    Rigidbody2D playerRb;
+    float moveForce = 10f;
+
 	// Use this for initialization
 	void Start () {
-	
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        if (player == null)
+        {
+            Debug.Log("Player not found.");
+        }
+        playerRb = player.GetComponent<Rigidbody2D>();
+        if (playerRb)
+        {
+            Debug.Log("Rigidbody not added to player.");
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
+        PlayerUpdate();
+        TileUpdate();
+
+    }
+
+    void setInputMode()
+    {
+
+    }
+
+    void PlayerUpdate()
+    {
+         if (player != null) {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+
+            playerRb.AddForce(moveForce * new Vector3(x, y, 0));
+
+
+        }
+
+        ////////////////////
+    }
+
+    void TileUpdate()
+    {
+
+    }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//just attach this script to player
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.tag = "Player";
+        gameObject.AddComponent<Rigidbody2D>();
+        gameObject.AddComponent<MeshCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
