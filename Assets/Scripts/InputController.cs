b217c5ba69577aa5500048080e2867b31755db49
Liam Atticus Clarke <liam.att.clarke@gmail.com>
2016-01-30 17:30:00 -0500

    more stuff

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -5,73 +5,76 @@ public enum Direction { Up, Down, Left, Right }
 
 public class InputController : MonoBehaviour {
 	
-    public float moveForce = 10f;
-    public float tileReleaseThreshold = 0.25f;
-    public float tileMoveThreshold = 0.25f;
-
-    enum inputMode { NONE, PLAYER, MAP }
-	inputMode mode { get; set; }
+	public float tileReleaseThreshold = 0.25f;
+	public float tileMoveThreshold = 0.25f;
+	
+	public enum inputMode { NONE, PLAYER, MAP }
+	public inputMode mode;
 	
 	Player player;
 	Rigidbody2D playerRb;
-    Map map;
-
-    bool isInputReleased = false;
-
+	Map map;
+	
+	bool isInputReleased = false;
+	
 	void Awake() {
-        map = GameObject.Find("Map").GetComponent<Map>();
-        player = GameObject.Find("Player").GetComponent<Player>();
-        playerRb = player.GetComponent<Rigidbody2D>();
-        mode = inputMode.MAP;
-    }
+		//map = GameObject.Find("Map").GetComponent<Map>();
+		mode = inputMode.PLAYER;
+	}
+	
+	void Start()
+	{
+		player = GameObject.Find("Player").GetComponent<Player>();
+		playerRb = player.GetComponent<Rigidbody2D>();
+	}
 	
 	void Update () {
-        float axisX = Mathf.Abs(Input.GetAxis("Horizontal"));
-        float axisY = Mathf.Abs(Input.GetAxis("Vertical"));
+		float axisX = Mathf.Abs(Input.GetAxis("Horizontal"));
+		float axisY = Mathf.Abs(Input.GetAxis("Vertical"));
 		if (axisX <= tileReleaseThreshold && axisY <= tileReleaseThreshold) {
-            isInputReleased = true;
-        }
+			isInputReleased = true;
+		}
 		if (mode == inputMode.PLAYER) {
 			PlayerUpdate ();
 		} else if (mode == inputMode.MAP) {
 			TileUpdate();
 		}    
 	}
-	
+	string str = "";
 	void PlayerUpdate()
 	{
 		if (player != null) {
 			float x = Input.GetAxis("Horizontal");
 			float y = Input.GetAxis("Vertical");
 			
-			playerRb.AddForce(moveForce * new Vector3(x, y, 0));
+			playerRb.AddForce(player.moveForce * new Vector3(x, y, 0));
+			//str += x + "\t";
+			//Debug.Log(str);
 		}
-		
-		////////////////////
 	}
 	
 	void TileUpdate()
 	{
-        if (isInputReleased) {
-            float axisX = Input.GetAxis("Horizontal");
-            float axisY = Input.GetAxis("Vertical");
-            if (axisX > tileMoveThreshold) {
-                // right
-                map.MoveTile(Direction.Right);
-                isInputReleased = false;
-            } else if (axisX < -tileMoveThreshold) {
-                // left
-                map.MoveTile(Direction.Left);
-                isInputReleased = false;
-            } else if (axisY > tileMoveThreshold) {
-                // up
-                map.MoveTile(Direction.Up);
-                isInputReleased = false;
-            } else if (axisY < -tileMoveThreshold) {
-                // down
-                map.MoveTile(Direction.Down);
-                isInputReleased = false;
-            }
-        }	
+		if (isInputReleased) {
+			float axisX = Input.GetAxis("Horizontal");
+			float axisY = Input.GetAxis("Vertical");
+			if (axisX > tileMoveThreshold) {
+				// right
+				map.MoveTile(Direction.Right);
+				isInputReleased = false;
+			} else if (axisX < -tileMoveThreshold) {
+				// left
+				map.MoveTile(Direction.Left);
+				isInputReleased = false;
+			} else if (axisY > tileMoveThreshold) {
+				// up
+				map.MoveTile(Direction.Up);
+				isInputReleased = false;
+			} else if (axisY < -tileMoveThreshold) {
+				// down
+				map.MoveTile(Direction.Down);
+				isInputReleased = false;
+			}
+		}	
 	}
 }
