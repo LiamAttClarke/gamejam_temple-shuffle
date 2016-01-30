91cff744eb645a129888e752dbdf9bfa987724a3
Liam Atticus Clarke <liam.att.clarke@gmail.com>
2016-01-30 00:05:46 -0500

    fixed merge conflict

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -2,29 +2,27 @@
 using System.Collections;
 
 public class InputController : MonoBehaviour {
-
-    enum inputMode { NONE, PLAYER, TILE }
-    inputMode mode = inputMode.PLAYER;
-
-    Player player;
-    Rigidbody2D playerRb;
-    float moveForce = 10f;
-
-	// Use this for initialization
+	
+	enum inputMode { NONE, PLAYER, TILE }
+	inputMode mode = inputMode.PLAYER;
+	
+	Player player;
+	Rigidbody2D playerRb;
+	float moveForce = 10f;
+	
 	void Start () {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        if (player == null)
-        {
-            Debug.Log("Player not found.");
-        }
-        playerRb = player.GetComponent<Rigidbody2D>();
-        if (playerRb)
-        {
-            Debug.Log("Rigidbody not added to player.");
-        }
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+		if (player == null)
+		{
+			Debug.Log("Player not found.");
+		}
+		playerRb = player.GetComponent<Rigidbody2D>();
+		if (playerRb)
+		{
+			Debug.Log("Rigidbody not added to player.");
+		}
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (mode == inputMode.PLAYER) {
 			PlayerUpdate ();
@@ -32,28 +30,26 @@ public class InputController : MonoBehaviour {
 			TileUpdate();
 		}    
 	}
-
-    void setInputMode()
-    {
-
-    }
-
-    void PlayerUpdate()
-    {
-         if (player != null) {
-            float x = Input.GetAxis("Horizontal");
-            float y = Input.GetAxis("Vertical");
-
-            playerRb.AddForce(moveForce * new Vector3(x, y, 0));
-
-
-        }
-
-        ////////////////////
-    }
-
-    void TileUpdate()
-    {
-		 
-    }
+	
+	void setInputMode()
+	{
+		
+	}
+	
+	void PlayerUpdate()
+	{
+		if (player != null) {
+			float x = Input.GetAxis("Horizontal");
+			float y = Input.GetAxis("Vertical");
+			
+			playerRb.AddForce(moveForce * new Vector3(x, y, 0));
+		}
+		
+		////////////////////
+	}
+	
+	void TileUpdate()
+	{
+		
+	}
 }
