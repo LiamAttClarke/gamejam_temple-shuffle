f943d602650d41d2f68f623c9393b362d6dbc9f0
Ian Nastajus <ian.nastajus@gmail.com>
2016-01-31 15:27:15 -0500

    platforms work

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+    public Sprite platformUp;
+    public Sprite platformDown;
+    bool active = false;
+
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        PolygonCollider2D pc = gameObject.GetComponent<PolygonCollider2D>();
+        if (pc == null)
+        {
+            Debug.Log("manually-created polygon collider intended to exist is missing on: " + gameObject.name);
+        }
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        if (platformUp == null)
+        {
+            Debug.Log("platformUp sprite missing on: " + gameObject.name);
+        }
+        if (platformDown == null)
+        {
+            Debug.Log("platformDown sprite missing on " + gameObject.name);
+        }
+        sr.sprite = platformUp;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player != null)
+        {
+            active = true;
+            sr.sprite = platformDown;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Player player = other.GetComponent<Player>();
+        if (player != null)
+        {
+            active = false;
+            sr.sprite = platformUp;
+        }
+    }
+
+}
--- Assets/Scripts/Triggerable.cs
+++ Assets/Scripts/Triggerable.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class Triggerable : MonoBehaviour {
 
+    protected bool active = false; 
+
 	void Awake () {
 
-        CircleCollider2D cc = gameObject.AddComponent<CircleCollider2D>();
+        PolygonCollider2D cc = gameObject.AddComponent<PolygonCollider2D>();
         cc.isTrigger = true;
-	
+        Debug.Log("parent");
 	}	
 }
