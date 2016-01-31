dcc448dfe1b11f8873635f445d4b3a7933ff3313
Liam Atticus Clarke <liam.att.clarke@gmail.com>
2016-01-30 23:35:45 -0500

    added teleport

--- Assets/Scripts/EdgePortal.cs
+++ Assets/Scripts/EdgePortal.cs
@@ -5,16 +5,18 @@ public class EdgePortal : MonoBehaviour {
 
 	public EdgePortal OppositePortal { get; private set; }
 
-	void Awake() {
-
-	}
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-
 	public void Init(EdgePortal oppositePortal) {
 		OppositePortal = oppositePortal;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag == "Player") {
+			// if on other side of portal, teleport to oppositePortal
+			Vector3 portalToPlayer = other.transform.position - transform.position;
+			float side = Vector3.Dot (transform.up, portalToPlayer);
+			if (side > 0) {
+				other.transform.position = OppositePortal.transform.position + portalToPlayer;
+			}
+		}
 	}
 }
